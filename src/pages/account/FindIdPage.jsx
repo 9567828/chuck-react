f1fe4b81c05c06300e8ad86ef542dc8d4cdaf608
9567828 <kimssi9567@gmail.com>
2025-02-17 01:15:46 +0900

    아이디찾기페이지 수정

--- src/pages/account/FindIdPage.jsx
+++ src/pages/account/FindIdPage.jsx
@@ -1,12 +1,10 @@
 import FindId from "../../components/account/FindId";
-import FindResult from "../../components/account/FindIdResult";
 import AccountFooter from "../../layouts/AccountFooter";
 
 function FindIdPage() {
   return (
     <>
       <FindId />
-      <FindResult />
       <AccountFooter />
     </>
   );
