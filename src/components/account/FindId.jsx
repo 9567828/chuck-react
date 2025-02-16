e8986fe68f0b8c5cd93897ed26fdaedeec7a1043
9567828 <kimssi9567@gmail.com>
2025-02-17 01:15:46 +0900

    아이디찾기페이지 수정

--- src/components/account/FindId.jsx
+++ src/components/account/FindId.jsx
@@ -1,16 +1,21 @@
 import { Link } from "react-router";
+import style from "../../assets/scss/style.module.scss";
+import ErrorMessage from "./../common/ErrorMessage";
 
 function FindId() {
+  // 결과화면 state훅으로 설정해서 UI 바꿔서 보여주도록
   return (
     <div className="sign-container">
-      <h1 className="title-xl font-bold join find-id-title">아이디 찾기</h1>
+      <h1 className={`${style.titleXl} ${style.fontBold} ${style.mb} find-id-title`}>아이디 찾기</h1>
       <form action="" id="findIdForm" className="find-id-form set-bottom">
         <div className="input-wrap">
           <div className="findId-wrap">
             <label htmlFor="inputFindId"></label>
             <input type="text" name="" id="inputFindId" className="input-box" placeholder="사번입력" />
           </div>
-          <p className="message info">소속된 회사의 사번을 입력해주세요.</p>
+          {/* state훅으로 설정하면 됨 */}
+          <p className={`${style.graySMfont} ${style.info}`}>소속된 회사의 사번을 입력해주세요.</p>
+          <ErrorMessage text={"입력하신 사번이 일치하지 않습니다"} />
         </div>
       </form>
       <button className="primary-btn long" form="findIdForm">
--- src/components/account/FindIdResult.jsx
+++ src/components/account/FindIdResult.jsx
@@ -9,7 +9,7 @@ function FindIdResult() {
       </div>
       <Link to="/login/find-pw" className="go-find-pw body-sm">
         <span>비밀번호 찾기</span>
-        <img src="/public/img/ic_move_in.svg" alt="화살표" />
+        <img src="/img/ic_move_in.svg" alt="화살표" />
       </Link>
     </div>
   );
