d074c1f51125ba4ccf89afc1c03f724f14115a51
9567828 <kimssi9567@gmail.com>
2025-02-21 21:27:42 +0900

    머지후 커밋

--- src/layouts/SideMenu.jsx
+++ src/layouts/SideMenu.jsx
@@ -1,93 +1,3 @@
-<<<<<<< HEAD
-import { Link } from "react-router";
-
-function SideMenu() {
-  return (
-    <aside>
-      <div className="lnb-container admin">
-        <h1 className="lnb-title font-bold body-md">관리자</h1>
-        <ul className="lnb">
-          <li className="lnb-menu">
-            <Link to="/admin/company-info" className="lnb-style lnb-menu-item">
-              <span>회사정보</span>
-            </Link>
-          </li>
-          <li className="lnb-menu">
-            <div className="lnb-style lnb-menu-item">
-              <span>출퇴근 관리</span>
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 11.825L8.825 8L5 4.175L6.18333 3L11.1833 8L6.18333 13L5 11.825Z" fill="#616161" />
-              </svg>
-            </div>
-            <ul className="lnb-submenu">
-              <li>
-                <Link to="/admin/work-time/state" className="lnb-style lnb-submenu-item">
-                  출퇴근 관리
-                </Link>
-              </li>
-              <li>
-                <Link to="/admin/work-time/setting" className="lnb-style lnb-submenu-item">
-                  출퇴근 설정
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li className="lnb-menu">
-            <div className="lnb-style lnb-menu-item">
-              <span>근무/휴가 관리</span>
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 11.825L8.825 8L5 4.175L6.18333 3L11.1833 8L6.18333 13L5 11.825Z" fill="#616161" />
-              </svg>
-            </div>
-            <ul className="lnb-submenu">
-              <li>
-                <Link to="" className="lnb-style lnb-submenu-item">
-                  하위메뉴
-                </Link>
-              </li>
-              <li>
-                <Link to="" className="lnb-style lnb-submenu-item">
-                  하위메뉴
-                </Link>
-              </li>
-              <li>
-                <Link to="" className="lnb-style lnb-submenu-item">
-                  하위메뉴
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li className="lnb-menu">
-            <div className="lnb-style lnb-menu-item">
-              <span>조직원 관리</span>
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M5 11.825L8.825 8L5 4.175L6.18333 3L11.1833 8L6.18333 13L5 11.825Z" fill="#616161" />
-              </svg>
-            </div>
-            <ul className="lnb-submenu">
-              <li>
-                <Link to="" className="lnb-style lnb-submenu-item">
-                  하위메뉴
-                </Link>
-              </li>
-              <li>
-                <Link to="" className="lnb-style lnb-submenu-item">
-                  하위메뉴
-                </Link>
-              </li>
-              <li>
-                <Link to="" className="lnb-style lnb-submenu-item">
-                  하위메뉴
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li className="lnb-menu">
-            <Link to="" className="lnb-style lnb-menu-item">
-              <span>환경 설정</span>
-            </Link>
-          </li>
-=======
 import { Link, NavLink } from "react-router";
 import style from "../assets/scss/modules/style.module.scss";
 import { useState, useEffect } from "react";
@@ -199,7 +109,6 @@ function SideMenu() {
               ) : null}
             </li>
           ))}
->>>>>>> a36c8db50f2765ec60586b5967f3531a89e5aed2
         </ul>
       </div>
       <div className="lnb-container my-profile">
