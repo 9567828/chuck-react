792d72b42df1ebb851bfbf50d5c861b1c76f1ec5
9567828 <kimssi9567@gmail.com>
2025-03-12 23:57:57 +0900

    수정중

--- src/api/holiday.js
+++ src/api/holiday.js
@@ -1,26 +1,31 @@
 import axios from "axios";
 
-const url = "https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getHoliDeInfo";
-const apiKey = import.meta.env.VITE_decoding_key;
-// const encodingKey = import.meta.env.VITE_encoding_key;
+const url = "https://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/";
+const decodedKey = import.meta.env.VITE_decoding_key;
+const encodingKey = import.meta.env.VITE_encoding_key;
 
 const getHoliday = async (year, month) => {
-  const encodingURL = encodeURIComponent(apiKey);
+  // let encodingURL = encodeURIComponent(decodedKey);
+  // encodingURL = encodingURL.replace(/\+/g, "%2B");
+  // // encodingURL = encodingURL.replace(/\//g, "%2F");
+  // // encodingURL = encodingURL.replace(/=/g, "%3D");
 
-  console.log(encodingURL);
+  console.log(encodingKey);
 
   const config = {
-    ServiceKey: encodingURL,
+    ServiceKey: encodingKey,
     solYear: year,
     solMonth: month,
     _type: "json",
   };
 
+  // const params = new URLSearchParams(config).toString();
+
   console.log(config);
 
   const res = await axios({
     method: "GET",
-    url: `${url}`,
+    url: `${url}getHoliDeInfo?`,
     params: config,
   });
 
